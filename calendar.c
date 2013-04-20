67900473ff4e01b0c3ac41c5266c6050df264e5d
Ntrm <m0rt3l.king+github@gmail.com>
2013-04-20 19:17:01 +0200

    Konflikt beseitigt

--- calendar.c
+++ calendar.c
@@ -10,60 +10,36 @@ TAppointment Calendar[MAX_APPOINTMENTS];
 
 void createAppointment()
 {
-<<<<<<< HEAD
-    printf("createAppointment()\n");
-=======
     printf("createAppointment()\n\n");
     waitForEnter();
->>>>>>> Modulheader wurden ergaenzt
 }
 
 void editAppointment()
 {
-<<<<<<< HEAD
-    printf("editAppointment()\n");
-=======
     printf("editAppointment()\n\n");
     waitForEnter();
->>>>>>> Modulheader wurden ergaenzt
 }
 
 void deleteAppointment()
 {
-<<<<<<< HEAD
-    printf("deleteAppointment()\n");
-=======
     printf("deleteAppointment()\n\n");
     waitForEnter();
->>>>>>> Modulheader wurden ergaenzt
 }
 
 void searchAppointment()
 {
-<<<<<<< HEAD
-    printf("searchAppointment()\n");
-=======
     printf("searchAppointment()\n\n");
     waitForEnter();
->>>>>>> Modulheader wurden ergaenzt
 }
 
 void sortCalendar()
 {
-<<<<<<< HEAD
-    printf("sortCalendar()\n");
-=======
     printf("sortCalendar()\n\n");
     waitForEnter();
->>>>>>> Modulheader wurden ergaenzt
 }
 
 void listCalendar()
 {
-<<<<<<< HEAD
-    printf("listCalendar()\n");
-=======
     printf("listCalendar()\n\n");
     waitForEnter();
->>>>>>> Modulheader wurden ergaenzt
 }
